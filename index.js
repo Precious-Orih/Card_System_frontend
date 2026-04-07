459ee0debe964cb34d49688fb5c1a5002bc4cb6d
Ehi-Orih <ehiorih25@gmail.com>
2026-04-07 07:35:18 +0100

    working on responsiveness

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 //Menu bar stuff
 const button = document.getElementById('toggle');
 const menubar = document.querySelector('.menu-bar');
+const user = document.getElementById('user');
 
 button.onclick = function () {
     menubar.classList.toggle('close');
 };
 window.addEventListener('load', (event) => {
-    menubar.classList.toggle('close', window.matchMedia('(min-width:769px) and (max-width:1024px)').matches);
-});
+    menubar.classList.toggle('close', window.matchMedia('(min-width:801px) and (max-width:1200px)').matches);
+    user.classList.toggle('hidden', window.matchMedia('(min-width:900px)').matches);
 
+});
 window.addEventListener('resize', () => {
-    menubar.classList.toggle('close', window.matchMedia('(min-width:769px) and (max-width:1024px)').matches);
+    menubar.classList.toggle('close', window.matchMedia('(min-width:801px) and (max-width:1200px)').matches);
+        user.classList.toggle('hidden', window.matchMedia('(min-width:900px)').matches);
+
 });
 
-// rhruehfi
+
+
+
